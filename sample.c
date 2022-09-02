ca236f092119684fe3d937759f53d280f055a96c
204g1a05b2 <94513337+204G1A05B2@users.noreply.github.com>
2022-09-02 15:11:52 +0530

    " C  Thrisharika Created Sample.c"

---
+++ sample.c
@@ -0,0 +1,6 @@
+/*Name Of the Candidate:C.Thrisharika
+Roll NO:204G1A05B2
+Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
